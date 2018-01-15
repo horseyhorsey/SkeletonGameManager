1f4b4232538acea708b9aaae6a0645b972c3e651
dave <dub.cyclone@gmail.com>
2018-01-15 19:08:22 +0000

    Add initial engine to deserialize skeleton game yaml files.

---
+++ Modules/SkeletonGame.Engine/ISkeletonGameAttract.cs
@@ -0,0 +1,36 @@
+﻿using SkeletonGame.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SkeletonGame.Engine
+{
+    public interface ISkeletonGameAttract
+    {
+        /// <summary>
+        /// Gets the available sequences. A sequence contains all types, but only one type is set at a time. Get the value where not null. Yaml sucks
+        /// </summary>
+        /// <param name="sequence">The sequence.</param>
+        /// <returns></returns>
+        IEnumerable<SequenceBase> GetAvailableSequences(AttractYaml attractYaml);
+    }
+
+    public class SkeletonGameAttract : ISkeletonGameAttract
+    {
+        public IEnumerable<SequenceBase> GetAvailableSequences(AttractYaml attractYaml)
+        {
+            List<SequenceBase> sequences = new List<SequenceBase>();
+
+            foreach (var seq in attractYaml.Sequence)
+            {
+                var notNullSequence = (SequenceBase)typeof(Sequence).GetProperties()
+                          .Select(prop => prop.GetValue(seq, null))
+                          .Where(val => val != null).First();
+
+                if (notNullSequence != null)
+                    sequences.Add(notNullSequence);
+            }
+
+            return sequences;                 
+        }
+    }
+}
---
+++ Modules/SkeletonGame.Engine/ISkeletonGameSerializer.cs
@@ -0,0 +1,89 @@
+﻿using SkeletonGame.Models;
+using System.IO;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace SkeletonGame.Engine
+{
+    public interface ISkeletonGameSerializer
+    {
+        /// <summary>
+        /// Converts a yaml file to json.
+        /// </summary>
+        /// <param name="yamlFile">The yaml file.</param>
+        /// <returns></returns>
+        string ConvertToJson(string yamlFile);
+
+        /// <summary>
+        /// Deserializes the yaml from given type
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="yamlFile">The yaml file.</param>
+        /// <returns></returns>
+        T DeserializeSkeletonYaml<T>(string yamlFile);        
+
+        /// <summary>
+        /// Serializes the game assets to yaml
+        /// </summary>
+        /// <param name="yamlFile">The yaml file.</param>
+        /// <param name="yamlObjectGraph">The asset file object.</param>
+        void SerializeYaml(string yamlFile, object yamlObjectGraph);
+
+    }
+
+    public class SkeletonGameSerializer : ISkeletonGameSerializer
+    {
+        /// <summary>
+        /// Deserializes the yaml from given type
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="yamlFile">The yaml file.</param>
+        /// <returns></returns>
+        public T DeserializeSkeletonYaml<T>(string yamlFile)
+        {
+            try
+            {
+                var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(new CamelCaseNamingConvention())
+                .Build();
+
+                using (TextReader reader = File.OpenText(yamlFile))
+                {
+                    var objects = deserializer.Deserialize<T>(reader);                    
+
+                    return objects;
+                }
+            }
+            catch (System.Exception ex)
+            {
+                ex.Data.Add("yaml", yamlFile);
+                throw;
+            }
+        }
+
+        public string ConvertToJson(string yamlFile)
+        {
+            using (TextReader reader = File.OpenText(yamlFile))
+            {
+                var deserializer = new DeserializerBuilder().Build();
+                var yamlObject = deserializer.Deserialize(reader);
+
+                var serializer = new SerializerBuilder()
+                    .JsonCompatible()
+                    .Build();
+
+                return serializer.Serialize(yamlObject);
+            }
+        }
+
+        public void SerializeYaml(string yamlFile, object yamlObjectGraph)
+        {
+            var yamlSerializer = new Serializer();
+
+            using (TextWriter writer = File.CreateText(yamlFile))
+            {
+                yamlSerializer.Serialize(writer, yamlObjectGraph);
+            }
+        }
+    }
+}
