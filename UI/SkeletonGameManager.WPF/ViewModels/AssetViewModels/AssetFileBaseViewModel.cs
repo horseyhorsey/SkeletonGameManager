d1c0d5d903e518749e0d4eb6e021654c63f0fa3d
dave <dub.cyclone@gmail.com>
2018-01-17 10:55:31 +0000

    Refactor AssetFileBaseVm to own file.

---
+++ UI/SkeletonGameManager.WPF/ViewModels/AssetViewModels/AssetFileBaseViewModel.cs
@@ -0,0 +1,33 @@
+﻿using GongSolutions.Wpf.DragDrop;
+using Prism.Mvvm;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace SkeletonGameManager.WPF.ViewModels.AssetViewModels
+{
+    public abstract class AssetFileBaseViewModel : BindableBase, IDropTarget
+    {
+        private ObservableCollection<string> assetFiles = new ObservableCollection<string>();
+        public ObservableCollection<string> AssetFiles
+        {
+            get { return assetFiles; }
+            set { SetProperty(ref assetFiles, value); }
+        }
+
+        public virtual void DragOver(IDropInfo dropInfo)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public virtual void Drop(IDropInfo dropInfo)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public virtual Task GetFiles()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
