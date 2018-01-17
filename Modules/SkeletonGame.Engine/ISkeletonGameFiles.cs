fbe1b025c5ff298789ebfac9165f6e90e158fcad
dave <dub.cyclone@gmail.com>
2018-01-17 02:22:27 +0000

    Add ISkeletonGameFiles to handle enumerating over files for given type

---
+++ Modules/SkeletonGame.Engine/ISkeletonGameFiles.cs
@@ -0,0 +1,34 @@
+﻿using SkeletonGame.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SkeletonGame.Engine
+{
+    public interface ISkeletonGameFiles
+    {
+        Task<IEnumerable<string>> GetFilesAsync(string path, AssetTypes assetType);
+        
+    }
+
+    public class SkeletonGameFiles : ISkeletonGameFiles
+    {
+        public async Task<IEnumerable<string>> GetFilesAsync(string path,  AssetTypes assetType)
+        {
+            if (!Directory.Exists(path)) throw new DirectoryNotFoundException($"Asset path not found {path} for type {assetType.ToString()}");
+
+            return await Task.Run(() =>
+            {
+                switch (assetType)
+                {
+                    case AssetTypes.Lampshows:
+                        return Directory.EnumerateFiles(path, @"*.lampshow");
+                    default:
+                        return null;
+                }
+            });
+            
+        }
+    }
+}
