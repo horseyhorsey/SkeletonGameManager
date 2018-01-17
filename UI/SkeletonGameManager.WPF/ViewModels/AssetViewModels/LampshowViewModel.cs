4fc5072067ff2d42e69e302938946decab8abd4e
dave <dub.cyclone@gmail.com>
2018-01-17 10:57:19 +0000

    Remove AssetFileBaseViewModel from LampshowVm

--- UI/SkeletonGameManager.WPF/ViewModels/AssetViewModels/LampshowViewModel.cs
+++ UI/SkeletonGameManager.WPF/ViewModels/AssetViewModels/LampshowViewModel.cs
@@ -163,24 +163,4 @@ namespace SkeletonGameManager.WPF.ViewModels.AssetViewModels
             
         }
     }
-
-    public abstract class AssetFileBaseViewModel : BindableBase, IDropTarget
-    {
-        private ObservableCollection<string> assetFiles = new ObservableCollection<string>();
-        public ObservableCollection<string> AssetFiles
-        {
-            get { return assetFiles; }
-            set { SetProperty(ref assetFiles, value); }
-        }
-
-        public virtual void DragOver(IDropInfo dropInfo)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public virtual void Drop(IDropInfo dropInfo)
-        {
-            throw new System.NotImplementedException();
-        }
-    }
 }
