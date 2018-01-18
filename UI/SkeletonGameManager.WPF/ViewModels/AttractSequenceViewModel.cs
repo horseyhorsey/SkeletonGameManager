621cda4e0e157ea45240cd6738bdc9a16b0d5282
dave <dub.cyclone@gmail.com>
2018-01-18 09:57:17 +0000

    + AttratSequenceViewModel

---
+++ UI/SkeletonGameManager.WPF/ViewModels/AttractSequenceViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Commands;
+using Prism.Mvvm;
+using SkeletonGame.Models;
+using SkeletonGameManager.WPF.Events;
+using SkeletonGameManager.WPF.Providers;
+using SkeletonGameManager.WPF.Views;
+using System.Windows.Input;
+
+namespace SkeletonGameManager.WPF.ViewModels
+{
+    public class AttractSequenceViewModel : BindableBase
+    {
+        public ISkeletonGameProvider _skeletonGameProvider { get; set; }
+
+        public ICommand SetAttractValueCommand { get; set; }
+
+        public AttractSequenceViewModel(SequenceBase sequence, ISkeletonGameProvider skeletonGameProvider)
+        {
+            _skeletonGameProvider = skeletonGameProvider;
+            
+            var type = sequence.GetType();
+            Name = type.Name;
+            Sequence = sequence;
+
+            SetAttractValueCommand = new DelegateCommand<string>((name) =>
+            {
+                var meh = Sequence;                
+                //AssetSelectDialogView dialog = new AssetSelectDialogView();
+
+                
+                //var vm = uc.Resolve(typeof(AssetSelectDialogViewModel));
+                //AssetSelectDialogViewModel vm = new AssetSelectDialogViewModel();
+                //dialog.ShowDialog();
+            });
+        }
+        
+        public string Name { get; set; }
+        public SequenceBase Sequence { get; set; }        
+    }
+}
