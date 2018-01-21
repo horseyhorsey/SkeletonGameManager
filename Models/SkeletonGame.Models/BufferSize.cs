1baaa9dbd36858f5159d66a2b9158fb83dfbb459
dave <dub.cyclone@gmail.com>
2018-01-21 07:22:45 +0000

    Add buffer size types

---
+++ Models/SkeletonGame.Models/BufferSize.cs
@@ -0,0 +1,11 @@
+﻿namespace SkeletonGame.Models
+{
+    public enum BufferSize
+    {
+        Buff256 = 256,
+        Buff512 = 512,
+        Buff1024 = 1024,
+        Buff2048 = 2048,
+        Buff4096 = 4096,
+    }
+}
