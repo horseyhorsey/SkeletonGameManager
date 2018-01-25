27eef76cc063a8695bc03d3887dd8cd2ccd711d2
dave <dub.cyclone@gmail.com>
2018-01-25 18:25:57 +0000

    Add drag dropping audio. Opening the current audio path for selected tab.

--- UI/SkeletonGameManager.WPF/ViewModels/AssetViewModels/SoundViewModel.cs
+++ UI/SkeletonGameManager.WPF/ViewModels/AssetViewModels/SoundViewModel.cs
@@ -1,11 +1,17 @@
-﻿using SkeletonGame.Engine;
+﻿using Prism.Commands;
+using SkeletonGame.Engine;
 using SkeletonGame.Models;
 using SkeletonGameManager.WPF.Providers;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using GongSolutions.Wpf.DragDrop;
+using System.Windows;
+using System.Linq;
 
 namespace SkeletonGameManager.WPF.ViewModels.AssetViewModels
 {
@@ -17,6 +23,14 @@ namespace SkeletonGameManager.WPF.ViewModels.AssetViewModels
         private AssetTypes _audioType;
 
         private Uri _audioPathFull;
+
+        private readonly string[] Extenstions = { ".mp3", ".ogg", ".wav"};
+        #endregion
+
+        #region Commands
+        public ICommand OpenSoundCommand { get; set; }
+        public ICommand OpenSoundDirectoryCommand { get; set; }
+        
         #endregion
 
         public SoundViewModel(ISkeletonGameFiles skeletonGameFiles, ISkeletonGameProvider skeletonGameProvider, AssetTypes audioType)
@@ -25,15 +39,88 @@ namespace SkeletonGameManager.WPF.ViewModels.AssetViewModels
             _skeletonGameProvider = skeletonGameProvider;
             _audioType = audioType;
 
+            OpenSoundCommand = new DelegateCommand<string>((s) =>
+            {
+                Process.Start(s);
+            });
+
+            OpenSoundDirectoryCommand = new DelegateCommand(() => Process.Start(_audioPathFull.AbsolutePath));
 
             InitCollectionByAudiotType(audioType);
         }
 
+        #region Properties
         private ObservableCollection<Music> music;
         public ObservableCollection<Music> AudioEntries
         {
             get { return music; }
             set { SetProperty(ref music, value); }
+        }
+        #endregion
+
+        #region Public Methods
+
+        public override void DragOver(IDropInfo dropInfo)
+        {
+            try
+            {
+                //Needs a few checks here. We can be dragging in from explorer or across to the datagrid.
+                var dragFileList = dropInfo.Data;
+
+                //Dragged from windows
+                if (dragFileList.GetType() == typeof(DataObject))
+                {
+                    var windowsFileList = ((DataObject)dropInfo.Data).GetFileDropList().Cast<string>();
+
+                    dropInfo.Effects = windowsFileList.Any(item =>
+                    {
+                        var extension = Path.GetExtension(item);
+                        return extension != null;
+                    }) ? DragDropEffects.Copy : DragDropEffects.None;
+                }
+                else
+                {
+                    dropInfo.Effects = DragDropEffects.Copy;
+                }
+
+            }
+            catch (System.Exception)
+            {
+            }
+        }
+
+        public override void Drop(IDropInfo dropInfo)
+        {
+            List<string> droppedFiles = new List<string>();
+
+            //Needs a few checks here. We can be dragging in from explorer or across to the datagrid.
+            var dragFileList = dropInfo.Data;
+
+            //Dragged files from windows
+            if (dragFileList.GetType() == typeof(DataObject))
+            {
+                var windowsFileList = ((DataObject)dropInfo.Data).GetFileDropList().Cast<string>();
+
+                droppedFiles.AddRange(windowsFileList);
+
+                // Remove any files that are not a valid extension for sound and check if not already existing in the file list.
+                droppedFiles.ForEach((file) => 
+                {
+                    var ext = Path.GetExtension(file);
+                    if (this.Extenstions.Any(x=> x == ext) && !this.AssetFiles.Any(x => x == file))
+                    {
+                        var newFilePath = Path.Combine(_audioPathFull.AbsolutePath, Path.GetFileName(file));
+                        //Copy the file to the directory and add to list
+                        if (!File.Exists(newFilePath))
+                        {
+                            File.Copy(file, newFilePath);
+
+                            this.AssetFiles.Add(file);
+                        }
+                        
+                    }
+                });
+            }
         }
 
         /// <summary>
@@ -60,7 +147,7 @@ namespace SkeletonGameManager.WPF.ViewModels.AssetViewModels
             }
 
             if (audioDir.Contains("."))
-                _audioPathFull = new Uri(Path.Combine(_skeletonGameProvider.GameFolder,  audioDir), UriKind.RelativeOrAbsolute);
+                _audioPathFull = new Uri(Path.Combine(_skeletonGameProvider.GameFolder, audioDir), UriKind.RelativeOrAbsolute);
             else
                 _audioPathFull = new Uri(audioDir);
 
@@ -68,7 +155,8 @@ namespace SkeletonGameManager.WPF.ViewModels.AssetViewModels
 
             var audioFiles = await _skeletonGameFiles.GetFilesAsync(_audioPathFull.AbsolutePath, _audioType);
             this.AssetFiles = new System.Collections.ObjectModel.ObservableCollection<string>(audioFiles);
-        }
+        } 
+        #endregion
 
         #region Private Methods
 
