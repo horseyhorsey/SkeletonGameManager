69bc66fa33fa0f2546a2c3df0476d25e3529f6ce
dave <dub.cyclone@gmail.com>
2018-02-03 09:38:36 +0000

    Viewmodels, used?

---
+++ UI/SkeletonGameManager.WPF/ViewModels/SequenceViewModelBase.cs
@@ -0,0 +1,35 @@
+﻿using Prism.Events;
+using SkeletonGame.Models;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace SkeletonGameManager.WPF.ViewModels
+{
+    public class SequenceViewModelBase : SkeletonGameManagerViewModelBase
+    {
+        public SequenceViewModelBase(IEventAggregator eventAggregator) : base(eventAggregator)
+        {
+
+        }
+
+        private ObservableCollection<SequenceBase> sequences;
+        public ObservableCollection<SequenceBase> Sequences
+        {
+            get { return sequences; }
+            set { SetProperty(ref sequences, value); }
+        }
+
+        private SequenceBase selectedSequence;
+
+        public SequenceBase SelectedSequence
+        {
+            get { return selectedSequence; }
+            set { SetProperty(ref selectedSequence, value); }
+        }
+
+        public override Task OnLoadYamlFilesChanged()
+        {
+            return null;
+        }
+    }
+}
