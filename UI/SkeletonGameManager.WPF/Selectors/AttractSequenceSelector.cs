c81e0fc6654475438d5859d01c7ab873e1a62ff4
horsetop <horselaptop>
2018-02-03 16:04:44 +0000

    Add ScriptedText to template selector

--- UI/SkeletonGameManager.WPF/Selectors/AttractSequenceSelector.cs
+++ UI/SkeletonGameManager.WPF/Selectors/AttractSequenceSelector.cs
@@ -52,6 +52,8 @@ namespace SkeletonGameManager.WPF.Selectors
                 return element.FindResource("Sequence_Panning") as DataTemplate;
             else if (type == typeof(MarkupLayer))
                 return element.FindResource("Sequence_Markup") as DataTemplate;
+            else if (type == typeof(ScriptedText))
+                return element.FindResource("Sequence_ScriptedText") as DataTemplate;
 
             else
                 return null;
