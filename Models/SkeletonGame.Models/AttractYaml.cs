acdbb5dd3e6db75dc33b6ed71c70a48889ef9cd2
dave <dub.cyclone@gmail.com>
2018-02-04 10:44:00 +0000

    Add constructors to models to init with a Sequence base.

--- Models/SkeletonGame.Models/AttractYaml.cs
+++ Models/SkeletonGame.Models/AttractYaml.cs
@@ -14,95 +14,33 @@ namespace SkeletonGame.Models
         List<string> TextList { get; set; }
 
         ObservableCollection<TextListViewModel> TextEntries { get; set; }
-
     }
 
     public class AttractYaml
     {
         [YamlMember(Alias = "Sequence", ApplyNamingConventions = false)]
-        public List<Sequence> AttractSequences { get; set; }
+        public ObservableCollection<Sequence> AttractSequences { get; set; }
 
         [YamlIgnore]
         public ObservableCollection<SequenceBase> Sequences { get; set; } = new ObservableCollection<SequenceBase>();
     }
 
     [PropertyChanged.AddINotifyPropertyChangedInterface]
-    public class SequenceBase
-    {
-        [YamlIgnore]
-        public string Name { get; set; }
-
-        public string lampshow { get; set; }
-        public decimal duration { get; set; } = 1.0m;
-
-        [YamlMember(Alias = "sound", ApplyNamingConventions = false)]
-        public string Sound { get; set; }
-
-
-        //[YamlMember(Alias = "Animation", ApplyNamingConventions = false)]
-        //public AnimationLayer AnimationLayer { get; set; } = null;
-
-        [YamlMember(Alias = "markup_layer", ApplyNamingConventions = false)]
-        public MarkupLayer MarkupLayer { get; set; } = null;
-    }
-
-    [PropertyChanged.AddINotifyPropertyChangedInterface]
-    public class Sequence
-    {
-        [YamlMember(Alias = "LastScores", ApplyNamingConventions = false)]
-        public LastScores LastScores { get; set; }
-
-        [YamlMember(Alias = "Combo", ApplyNamingConventions = false)]
-        public Combo Combo { get; set; }
-
-        [YamlMember(Alias = "text_layer", ApplyNamingConventions = false)]
-        public TextLayer text_layer { get; set; }
-
-        [YamlMember(Alias = "panning_layer", ApplyNamingConventions = false)]
-        public PanningLayer panning_layer { get; set; }
-
-        [YamlMember(Alias = "RandomText", ApplyNamingConventions = false)]
-        public RandomText RandomText { get; set; }
-
-        [YamlMember(Alias = "Animation", ApplyNamingConventions = false)]
-        public AttractAnimation AttractAnimation { get; set; }
-
-        [YamlMember(Alias = "HighScores", ApplyNamingConventions = false)]
-        public HighScores HighScores { get; set; }
-
-        [YamlMember(Alias = "Credits", ApplyNamingConventions = false)]
-        public Credits Credits { get; set; }
-
-        [YamlMember(Alias = "markup_layer", ApplyNamingConventions = false)]
-        public MarkupLayer MarkupLayer { get; set; }
-
-        [YamlMember(Alias = "group_layer", ApplyNamingConventions = false)]
-        public GroupLayer GroupLayer { get; set; }
-
-        [YamlMember(Alias = "ScriptedText", ApplyNamingConventions = false)]
-        public ScriptedText ScriptedText { get; set; }
-    }
-
-    public class ScriptedText : SequenceBase
+    public class ScriptedText : SequenceTextAnimationBase
     {
         [YamlMember(Alias = "Name", ApplyNamingConventions = false)]
         public new string Name { get; set; }
-
-        [YamlMember(Alias = "Font", ApplyNamingConventions = false)]
-        public string Font { get; set; }
-
-        [YamlMember(Alias = "FontStyle", ApplyNamingConventions = false)]
-        public string FontStyle { get; set; }
-
-        [YamlMember(Alias = "Animation", ApplyNamingConventions = false)]
-        public string Animation { get; set; }
 
         [YamlMember(Alias = "flashing", ApplyNamingConventions = false)]
         public bool Flashing { get; set; }
 
         [YamlMember(Alias = "TextOptions", ApplyNamingConventions = false)]
-        public List<TextOption> TextOptions { get; set; }
+        public ObservableCollection<TextOption> TextOptions { get; set; } = new ObservableCollection<TextOption>();
 
+        public ScriptedText()
+        {
+            this.SeqType = AttractSequenceType.ScriptedText;
+        }
     }
 
     public class Message
@@ -112,31 +50,18 @@ namespace SkeletonGame.Models
     }
 
 
-    public class LastScores : SequenceBase
+    public class LastScores : SequenceTextAnimationBase
     {
-        [YamlMember(Alias = "Font", ApplyNamingConventions = false)]
-        public string Font { get; set; }
-
-        [YamlMember(Alias = "FontStyle", ApplyNamingConventions = false)]
-        public string FontStyle { get; set; }
-
-        [YamlMember(Alias = "Animation", ApplyNamingConventions = false)]
-        public string Animation { get; set; }
+        public LastScores()
+        {
+            this.SeqType = AttractSequenceType.LastScores;
+        }
     }
 
     //C:\P-ROC\Games\jaws
     [PropertyChanged.AddINotifyPropertyChangedInterface]
-    public class Combo : SequenceBase, ITextEntries
+    public class Combo : SequenceTextAnimationBase, ITextEntries
     {
-        [YamlMember(Alias = "Animation", ApplyNamingConventions = false)]
-        public string Animation { get; set; }
-
-        [YamlMember(Alias = "Font", ApplyNamingConventions = false)]
-        public string Font { get; set; } = string.Empty;
-
-        [YamlMember(Alias = "FontStyle", ApplyNamingConventions = false)]
-        public string FontStyle { get; set; }
-
         private List<string> _textList;
         [YamlMember(Alias = "Text", ApplyNamingConventions = false)]
         public List<string> TextList
@@ -167,80 +92,130 @@ namespace SkeletonGame.Models
 
         [YamlIgnore]
         public ObservableCollection<TextListViewModel> TextEntries { get; set; } = new ObservableCollection<TextListViewModel>();
+
+        public Combo()
+        {
+            this.SeqType = AttractSequenceType.Combo;
+        }
     }
-    
+
+
+    [PropertyChanged.AddINotifyPropertyChangedInterface]
+    public class TextOption : ITextEntries
+    {
+        private List<string> _textList;
+        [YamlMember(Alias = "Text", ApplyNamingConventions = false)]
+        public List<string> TextList
+        {
+            get
+            {
+                return _textList;
+            }
+
+            set
+            {
+                _textList = value;
+
+                if (_textList != null)
+                {
+                    TextEntries.Clear();
+                    foreach (var textLine in _textList)
+                    {
+                        TextEntries.Add(new TextListViewModel { TextLine = textLine });
+                    }
+                }
+                else
+                {
+                    TextEntries.Clear();
+                }
+            }
+        }
+
+        [YamlIgnore]
+        public ObservableCollection<TextListViewModel> TextEntries { get; set; } =
+        new ObservableCollection<TextListViewModel>();
+
+    }
+
     public class TextListViewModel
     {
-        public string TextLine { get; set; }
+        public string TextLine { get; set; } = "";
     }
 
     [PropertyChanged.AddINotifyPropertyChangedInterface]
-    public class TextLayer : SequenceBase
+    public class TextLayer : SequenceTextBase
     {
         [YamlMember(Alias = "x", ApplyNamingConventions = false)]
-        public string X { get; set; }
+        public int X { get; set; } = 0;
 
         [YamlMember(Alias = "y", ApplyNamingConventions = false)]
-        public string Y { get; set; }
+        public int Y { get; set; }
+
+        [YamlMember(Alias = "width", ApplyNamingConventions = false, SerializeAs = typeof(string))]
+        public int Width { get; set; } = 0;
+
+        [YamlMember(Alias = "height", ApplyNamingConventions = false, SerializeAs = typeof(string))]
+        public int Height { get; set; } = 0;
 
         [YamlMember(Alias = "v_justify", ApplyNamingConventions = false, SerializeAs = typeof(string))]
-        public VJustify VJustify { get; set; }
+        public VJustify VJustify { get; set; } = VJustify.top;
 
         [YamlMember(Alias = "h_justify", ApplyNamingConventions = false, SerializeAs = typeof(string))]
-        public HJustify HJustify { get; set; }
+        public HJustify HJustify { get; set; } = HJustify.left;
 
         [YamlMember(Alias = "Text", ApplyNamingConventions = false)]
-        public string Text { get; set; }
-
-        [YamlMember(Alias = "Font", ApplyNamingConventions = false)]
-        public string Font { get; set; }
-
-        [YamlMember(Alias = "Background", ApplyNamingConventions = false)]
-        public string Animation { get; set; }
+        public string Text { get; set; } = "TEXT LAYER";
 
         [YamlMember(Alias = "blink_frames", ApplyNamingConventions = false)]
         public string BlinkFrames { get; set; }
 
-        [YamlMember(Alias = "FontStyle", ApplyNamingConventions = false)]
-        public string FontStyle { get; set; }
+        public TextLayer()
+        {
+            this.SeqType = AttractSequenceType.TextLayer;
+        }
 
     }
 
     [PropertyChanged.AddINotifyPropertyChangedInterface]
-    public class RandomText : SequenceBase
+    public class RandomText : SequenceTextAnimationBase
     {
         [YamlMember(Alias = "Header", ApplyNamingConventions = false)]
         public string Header { get; set; }
 
         [YamlMember(Alias = "TextOptions", ApplyNamingConventions = false)]
-        public List<TextOption> TextOptions { get; set; }
+        public ObservableCollection<TextOption> TextOptions { get; set; } = new ObservableCollection<TextOption>();
 
-        [YamlMember(Alias = "Font", ApplyNamingConventions = false)]
-        public string Font { get; set; }
-
-        [YamlMember(Alias = "Animation", ApplyNamingConventions = false)]
-        public string Animation { get; set; }
-
-        [YamlMember(Alias = "FontStyle", ApplyNamingConventions = false)]
-        public string FontStyle { get; set; }
+        public RandomText()
+        {
+            this.SeqType = AttractSequenceType.RandomText;
+        }
 
     }
 
     [PropertyChanged.AddINotifyPropertyChangedInterface]
     public class AnimationLayer
     {
-        [YamlMember(Alias = "Name", ApplyNamingConventions = false)]
+        [YamlMember(Alias = "name", ApplyNamingConventions = false)]
         public string Name { get; set; }
     }
 
     [PropertyChanged.AddINotifyPropertyChangedInterface]
     public class Content : SequenceBase
-    {
-        [YamlMember(Alias = "Animation", ApplyNamingConventions = false)]
-        public AnimationLayer AnimationLayer { get; set; }
+    {        
+        [YamlMember(Alias = "animation_layer", ApplyNamingConventions = false)]
+        public AttractAnimation animation_layer { get; set; }
+
+        [YamlMember(Alias = "group_layer", ApplyNamingConventions = false)]
+        public GroupLayer group_layer { get; set; }
 
         [YamlMember(Alias = "markup_layer", ApplyNamingConventions = false)]
-        public MarkupLayer MarkupLayer { get; set; }
+        public MarkupLayer markup_layer { get; set; }
+
+        [YamlMember(Alias = "text_layer", ApplyNamingConventions = false)]
+        public TextLayer text_layer { get; set; }
+
+        [YamlMember(Alias = "Combo", ApplyNamingConventions = false)]
+        public Combo combo_layer { get; set; }
     }
 
     public abstract class GroupBaseLayer : SequenceBase
@@ -250,6 +225,9 @@ namespace SkeletonGame.Models
     [PropertyChanged.AddINotifyPropertyChangedInterface]
     public class GroupLayer : GroupBaseLayer
     {
+        [YamlMember(Alias = "Name", ApplyNamingConventions = false)]
+        public string Name { get; set; }
+
         [YamlMember(Alias = "width", ApplyNamingConventions = false)]
         public string Width { get; set; }
 
@@ -263,46 +241,60 @@ namespace SkeletonGame.Models
         public byte[] FillColor { get; set; } = null;
 
         [YamlMember(Alias = "contents", ApplyNamingConventions = false)]
-        public List<Content> Layers { get; set; } = new List<Content>();
+        public ObservableCollection<Content> Contents { get; set; } = new ObservableCollection<Content>();
+
+        public GroupLayer()
+        {
+            this.SeqType = AttractSequenceType.GroupLayer;
+        }
     }
 
     [PropertyChanged.AddINotifyPropertyChangedInterface]
     public class Contents
     {
-        [YamlMember(Alias = "group_layer", ApplyNamingConventions = false)]
-        public GroupLayer GroupedLayer { get; set; } = new GroupLayer();
-    }
-
-    [PropertyChanged.AddINotifyPropertyChangedInterface]
-    public class TextOption : ITextEntries
-    {
-        [YamlMember(Alias = "Text", ApplyNamingConventions = false)]
-        public List<string> TextList { get; set; }
-
-        public ObservableCollection<TextListViewModel> TextEntries { get; set; }
+        [YamlMember(Alias = "contents", ApplyNamingConventions = false)]
+        public ObservableCollection<Content> Content { get; set; } = new ObservableCollection<Content>();
     }
 
     [PropertyChanged.AddINotifyPropertyChangedInterface]
     public class AttractAnimation : SequenceBase
     {
-        [YamlMember(Alias = "Name", ApplyNamingConventions = false)]
-        public new string Name { get; set; }
+        [YamlMember(Alias = "name", ApplyNamingConventions = false)]
+        public new string Name { get; set; } = "missing";
+
+        [YamlMember(Alias = "x", ApplyNamingConventions = false)]
+        public int X { get; set; } = 0;
+
+        [YamlMember(Alias = "y", ApplyNamingConventions = false)]
+        public int Y { get; set; }
+
+        [YamlMember(Alias = "repeat", ApplyNamingConventions = false)]
+        public bool Repeat { get; set; }
+
+        [YamlMember(Alias = "hold_last_frame", ApplyNamingConventions = false)]
+        public bool Hold { get; set; }
+
+        [YamlMember(Alias = "opaque", ApplyNamingConventions = false)]
+        public bool Opaque { get; set; }
+
+        public AttractAnimation()
+        {
+            this.Name = "missing";
+            this.SeqType = AttractSequenceType.Animation;
+        }
     }
 
     [PropertyChanged.AddINotifyPropertyChangedInterface]
-    public class HighScores : SequenceBase
+    public class HighScores : SequenceTextAnimationBase
     {
-        [YamlMember(Alias = "Font", ApplyNamingConventions = false)]
-        public string Font { get; set; }
-
-        [YamlMember(Alias = "FontStyle", ApplyNamingConventions = false)]
-        public string FontStyle { get; set; }
-
-        [YamlMember(Alias = "Animation", ApplyNamingConventions = false)]
-        public string Animation { get; set; }
 
         [YamlMember(Alias = "Order", ApplyNamingConventions = false)]
         public List<string> Order { get; set; }
+
+        public HighScores()
+        {
+            this.SeqType = AttractSequenceType.HighScores;
+        }
     }
 
     [PropertyChanged.AddINotifyPropertyChangedInterface]
@@ -313,5 +305,10 @@ namespace SkeletonGame.Models
 
         [YamlMember(Alias = "Animation", ApplyNamingConventions = false)]
         public string Animation { get; set; }
+
+        public Credits()
+        {
+            this.SeqType = AttractSequenceType.Credits;
+        }
     }
 }
