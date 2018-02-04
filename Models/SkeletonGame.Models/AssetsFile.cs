70ca199d1a8d4caa5bf20b5af24e0dbdd7fe5c6a
dave <dub.cyclone@gmail.com>
2018-02-04 10:41:15 +0000

    Make anims observable

--- Models/SkeletonGame.Models/AssetsFile.cs
+++ Models/SkeletonGame.Models/AssetsFile.cs
@@ -14,7 +14,7 @@ namespace SkeletonGame.Models
         public List<LampShow> LampShows { get; set; } = new List<LampShow>();
 
         [YamlMember(Alias = "Animations", ApplyNamingConventions = false)]
-        public ObservableCollection<Animation> Animations { get; set; } = new ObservableCollection<Animation>();
+        public List<Animation> Animations { get; set; } = new List<Animation>();
 
         [YamlMember(Alias = "Fonts", ApplyNamingConventions = false)]
         public Font Fonts { get; set; }
