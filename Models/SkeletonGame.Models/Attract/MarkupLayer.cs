22125038283f7ad50e38e69cd7f6499c48e53892
dave <dub.cyclone@gmail.com>
2018-02-04 10:40:36 +0000

    Change Message to List of TextList. Add constructor for defining type.

--- Models/SkeletonGame.Models/Attract/MarkupLayer.cs
+++ Models/SkeletonGame.Models/Attract/MarkupLayer.cs
@@ -1,24 +1,59 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using YamlDotNet.Serialization;
 
 namespace SkeletonGame.Models.Attract
 {
+    [PropertyChanged.AddINotifyPropertyChangedInterface]
     public class MarkupLayer : SequenceBase
     {
         [YamlMember(Alias = "width", ApplyNamingConventions = false)]
         public string Width { get; set; }
 
         [YamlMember(Alias = "Bold", ApplyNamingConventions = false)]
-        public Bold Bold { get; set; }
+        public Bold Bold { get; set; } = new Bold() { Font = "default" };
 
         [YamlMember(Alias = "Normal", ApplyNamingConventions = false)]
-        public Normal Normal { get; set; }
+        public Normal Normal { get; set; } = new Normal() { Font = "default" };
 
+        private List<string> _textList = new List<string>() { "" };
         [YamlMember(Alias = "Message", ApplyNamingConventions = false)]
-        public List<string> Message { get; set; }
+        public List<string> TextList
+        {
+            get
+            {
+                return _textList;
+            }
+
+            set
+            {
+                _textList = value;
+
+                if (_textList != null)
+                {
+                    TextEntries.Clear();
+                    foreach (var textLine in _textList)
+                    {
+                        TextEntries.Add(new TextListViewModel { TextLine = textLine });
+                    }
+                }
+                else
+                {
+                    TextEntries.Clear();
+                }
+            }
+        }
+
+        [YamlIgnore]
+        public ObservableCollection<TextListViewModel> TextEntries { get; set; } = new ObservableCollection<TextListViewModel>();
 
         [YamlMember(Alias = "Animation", ApplyNamingConventions = false)]
         public string Animation { get; set; }
+
+        public MarkupLayer()
+        {
+            this.SeqType = AttractSequenceType.MarkupLayer;
+        }
     }
 
     public class Bold
