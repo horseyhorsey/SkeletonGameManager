d8ae65cb893789eb8c6e44cd61cb1623fbb2111c
dave <dub.cyclone@gmail.com>
2018-02-04 10:41:37 +0000

    Reorder Sequence types alphabetically

--- Models/SkeletonGame.Models/AttractSequenceType.cs
+++ Models/SkeletonGame.Models/AttractSequenceType.cs
@@ -2,14 +2,16 @@
 {
     public enum AttractSequenceType
     {
-        LastScores,
-        Combo,
-        text_layer,
-        panning_layer,
-        RandomText,
         Animation,
-        HighScores,
+        Combo,
         Credits,
-        MarkupLayer
+        GroupLayer,
+        HighScores,
+        LastScores,
+        MarkupLayer,
+        PanningLayer,
+        RandomText,
+        ScriptedText,
+        TextLayer,
     }
 }
