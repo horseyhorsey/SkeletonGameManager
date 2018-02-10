1669ccee2fd00d427c27a30982e5890e7fa687ca
dave <dub.cyclone@gmail.com>
2018-02-10 20:08:50 +0000

    Add IFadeLayer interface

---
+++ Models/SkeletonGame.Models/Transition/IFadeLayer.cs
@@ -0,0 +1,20 @@
+﻿using YamlDotNet.Serialization;
+
+namespace SkeletonGame.Models.Transition
+{
+    public interface IFadeLayer
+    {        
+        string FadeType { get; set; }
+
+        int FrameCount { get; set; }
+    }
+
+    public class FadeLayer : IFadeLayer
+    {
+        [YamlMember(Alias = "param", ApplyNamingConventions = false, SerializeAs = typeof(string), ScalarStyle = YamlDotNet.Core.ScalarStyle.SingleQuoted)]
+        public string FadeType { get; set; } = "off";
+
+        [YamlMember(Alias = "frame_count", ApplyNamingConventions = false)]
+        public int FrameCount { get; set; } = 25;
+    }
+}
