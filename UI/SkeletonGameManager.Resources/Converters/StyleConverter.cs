e6b9e4277ebbe38257bd3c449d6df88074b4575e
dave <dub.cyclone@gmail.com>
2018-02-12 22:51:22 +0000

    Adjust style converter to create the name of the style its trying to retrieve.

--- UI/SkeletonGameManager.Resources/Converters/StyleConverter.cs
+++ UI/SkeletonGameManager.Resources/Converters/StyleConverter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SkeletonGame.Models;
+using System;
 using System.Windows;
 using System.Windows.Data;
 
@@ -9,12 +10,19 @@ namespace SkeletonGameManager.Resources.Converters
         public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             FrameworkElement targetElement = values[0] as FrameworkElement;
-            string styleName = values[1] as string;
 
-            if (styleName == null)
+            string styleName;
+            if (values[1] != null)
+            {
+                styleName = values[1].ToString();
+
+                if (styleName == null)
+                    return null;
+            }
+            else
                 return null;
 
-            Style newStyle = (Style)targetElement.TryFindResource(styleName);
+            Style newStyle = (Style)targetElement.TryFindResource(styleName + "SequenceStyle");
 
             if (newStyle == null)
                 newStyle = (Style)targetElement.TryFindResource("GroupLayerSequenceStyle");
