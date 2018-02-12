e921f19a05d45ab711717d2bc31910e9fec67ad0
dave <dub.cyclone@gmail.com>
2018-02-12 22:25:58 +0000

    Add Transitionout params to ScriptedText

--- Models/SkeletonGame.Models/Layers/ScriptedText.cs
+++ Models/SkeletonGame.Models/Layers/ScriptedText.cs
@@ -22,12 +22,24 @@ namespace SkeletonGame.Models.Layers
         [YamlMember(Alias = "trans_length", ApplyNamingConventions = false)]
         public int TransLength { get; set; } = 30;
 
+        [YamlMember(Alias = "transition_out", ApplyNamingConventions = false, SerializeAs = typeof(string))]
+        public TransitionType TransitionOut { get; set; } = TransitionType.None;
+
+        [YamlMember(Alias = "trans_out_param", ApplyNamingConventions = false, SerializeAs = typeof(string))]
+        public TransitionParam TransOutParam { get; set; } = TransitionParam.None;
+
+        [YamlMember(Alias = "trans_out_length", ApplyNamingConventions = false)]
+        public int TransOutLength { get; set; } = 30;
+
+        [YamlMember(Alias = "trans_out_duration", ApplyNamingConventions = false, SerializeAs = typeof(string))]
+        public decimal TransOutDuration { get; set; } = 1.0m;
+
         [YamlMember(Alias = "TextOptions", ApplyNamingConventions = false)]
         public ObservableCollection<TextOption> TextOptions { get; set; } = new ObservableCollection<TextOption>();
 
         public ScriptedText()
         {
-            this.SeqType = SequenceType.ScriptedText;
+            this.SeqType = SequenceType.ScriptedText;            
         }
     }
 }
