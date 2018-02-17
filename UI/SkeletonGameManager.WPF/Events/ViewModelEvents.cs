6eeb347a7ba4994c7eddd0dfc596a9bf3da76806
dave <dub.cyclone@gmail.com>
2018-02-17 15:22:31 +0000

    Add OnGameEndedEvent, OnLaunchGameEvent

--- UI/SkeletonGameManager.WPF/Events/ViewModelEvents.cs
+++ UI/SkeletonGameManager.WPF/Events/ViewModelEvents.cs
@@ -5,8 +5,11 @@ using SkeletonGameManager.WPF.Model;
 
 namespace SkeletonGameManager.WPF.Events
 {
-    public class LoadYamlFilesChanged : PubSubEvent<object> { }
+    public class LoadYamlFilesChanged : PubSubEvent<object> { }    
+    public class OnGameEndedEvent : PubSubEvent<bool> { }
+    public class OnLaunchGameEvent : PubSubEvent<object> { }
     public class VideoProcessItemRemoveEvent : PubSubEvent<SceneProcessItemViewModel> { }
     public class VideoProcessItemAddedEvent : PubSubEvent<TrimVideo> { }
-    public class VideoSourceEvent : PubSubEvent<string> { }
+    public class VideoSourceEvent : PubSubEvent<string> { }    
+    
 }
