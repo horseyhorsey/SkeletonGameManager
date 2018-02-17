bddce7b0c46fea4e7bee97dcee03d4e7e0b328ae
dave <dub.cyclone@gmail.com>
2018-02-17 15:22:11 +0000

    Created PlaybackItemViewModel, add DoubleToTimespanConverter into app.xaml

---
+++ UI/SkeletonGameManager.WPF/ViewModels/PlaybackItemViewModel.cs
@@ -0,0 +1,28 @@
+﻿using SkeletonGame.Engine;
+using SkeletonGame.Models;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace SkeletonGameManager.WPF.ViewModels
+{
+    [PropertyChanged.AddINotifyPropertyChangedInterface]
+    public class PlaybackItemViewModel
+    {
+        public PlaybackItemViewModel(string playBackFile)
+        {
+            PlaybackFile = playBackFile;
+        }
+
+        public string PlaybackFile { get; set; }
+
+        public ObservableCollection<PlayBackItem> PlayBackItems { get; set; }
+
+        public void UpdatePlayBackItems(bool orderByDescending = false)
+        {
+            PlayBackItems = new ObservableCollection<PlayBackItem>(RecordingManager.ParsePlaybackFile(PlaybackFile));
+
+            if (orderByDescending)
+                PlayBackItems = new ObservableCollection<PlayBackItem>(PlayBackItems.OrderByDescending(x => x.Event));
+        }
+    }
+}
