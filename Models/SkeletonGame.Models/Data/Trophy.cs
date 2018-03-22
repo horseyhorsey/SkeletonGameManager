dffa65b124e06da02090a47deac67e98108aa799
dave <dub.cyclone@gmail.com>
2018-03-22 17:16:03 +0000

    Add trophy and gamedata to models

---
+++ Models/SkeletonGame.Models/Data/Trophy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using YamlDotNet.Serialization;
+
+namespace SkeletonGame.Models.Data
+{
+    public class Trophy
+    {
+        [YamlMember(Alias = "description", ApplyNamingConventions = false)]
+        public string Description { get; set; }
+
+        [YamlMember(Alias = "completedDate", ApplyNamingConventions = false)]
+        public string CompletedDate { get; set; }
+
+        [YamlMember(Alias = "icon", ApplyNamingConventions = false)]
+        public string Icon { get; set; }
+    }
+
+    public class TrophyData
+    {
+        [YamlMember(Alias = "Trophys", ApplyNamingConventions = false)]
+        public Dictionary<string, Trophy> Trophys { get; set; }
+    }
+
+
+}
---
+++ Models/SkeletonGame.Models/GameData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using YamlDotNet.Serialization;
+
+namespace SkeletonGame.Models
+{
+    public class GameData
+    {
+        [YamlMember(Alias = "Audits", ApplyNamingConventions = false)]
+        public Dictionary<string, string> Audits { get; set; }
+    }
+}
