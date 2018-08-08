f1534f1b1f3a3afedd7c67ac50156015860b7561
dave <dub.cyclone@gmail.com>
2018-08-08 22:14:24 +0100

    Register the appsetting from MenuModule

--- UI/SkeletonGameManager.Module.Menus/MenusModule.cs
+++ UI/SkeletonGameManager.Module.Menus/MenusModule.cs
@@ -1,21 +1,28 @@
 ﻿using SkeletonGameManager.Module.Menus.Views;
 using Prism.Modularity;
 using Prism.Regions;
+using Microsoft.Practices.Unity;
+using SkeletonGameManager.Module.Menus.Model;
+using SkeletonGameManager.Base.Interface;
 
 namespace SkeletonGameManager.Module.Menus
 {
     public class MenusModule : IModule
     {
         private IRegionManager _regionManager;
+        private IUnityContainer _unityContainer;
 
-        public MenusModule(IRegionManager regionManager)
+        public MenusModule(IRegionManager regionManager, IUnityContainer unityContainer)
         {
             _regionManager = regionManager;
+            _unityContainer = unityContainer;
+            _unityContainer.RegisterInstance<IAppSettingsModel>(_unityContainer.Resolve<AppSettingsModel>(), new ContainerControlledLifetimeManager());
         }
 
         public void Initialize()
         {            
             _regionManager.RegisterViewWithRegion("FileMenuRegion", typeof(FileMenuView));
+                       
         }
     }
 }
