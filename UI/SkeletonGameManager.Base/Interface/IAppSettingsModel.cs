f37c85e166563186f75bf56905757008e9b77406
dave <dub.cyclone@gmail.com>
2018-08-08 22:13:29 +0100

    Add interface for UI settings.

---
+++ UI/SkeletonGameManager.Base/Interface/IAppSettingsModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace SkeletonGameManager.Base.Interface
+{
+    public interface IAppSettingsModel
+    {
+        ObservableCollection<string> RecentDirectories { get; set; }
+        IList<string> Load();
+        void Save();
+        void AddRecentDirectory(string gameFolder);
+    }
+
+}
---
+++ UI/SkeletonGameManager.Module.Menus/Model/AppSettingsModel.cs
@@ -0,0 +1,57 @@
+﻿using SkeletonGameManager.Base.Interface;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace SkeletonGameManager.Module.Menus.Model
+{
+    [PropertyChanged.AddINotifyPropertyChangedInterface]
+    public class AppSettingsModel : IAppSettingsModel
+    {
+        public ObservableCollection<string> RecentDirectories { get; set; }
+
+        public AppSettingsModel()
+        {
+            RecentDirectories = new ObservableCollection<string>();
+        }
+
+        public IList<string> Load()
+        {            
+            RecentDirectories.Add(Properties.Settings.Default.RecentDir5);
+            RecentDirectories.Add(Properties.Settings.Default.RecentDir4);
+            RecentDirectories.Add(Properties.Settings.Default.RecentDir3);
+            RecentDirectories.Add(Properties.Settings.Default.RecentDir2);
+            RecentDirectories.Add(Properties.Settings.Default.RecentDir1);
+
+            return RecentDirectories;
+        }
+
+        public void Save()
+        {            
+            Properties.Settings.Default.RecentDir1 = RecentDirectories[4];
+            Properties.Settings.Default.RecentDir2 = RecentDirectories[3];
+            Properties.Settings.Default.RecentDir3 = RecentDirectories[2];
+            Properties.Settings.Default.RecentDir4 = RecentDirectories[1];
+            Properties.Settings.Default.RecentDir5 = RecentDirectories[0];
+            Properties.Settings.Default.Save();
+        }
+
+        public void AddRecentDirectory(string gameFolder)
+        {
+            //Set recent directory and save
+            if (RecentDirectories?.Count > 0)
+            {
+                //Check if folder exists
+                var dupeFolder = RecentDirectories.FirstOrDefault(x => x == gameFolder);
+                if (dupeFolder == null)
+                    RecentDirectories.RemoveAt(RecentDirectories.Count - 1);
+                else
+                    RecentDirectories.Remove(gameFolder);
+
+                RecentDirectories.Insert(0, gameFolder);
+            }
+
+            Save();
+        }
+    }
+}
