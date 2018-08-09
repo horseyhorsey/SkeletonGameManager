906a894f8c93acee4a7ea1004ae36e7ced0f2b7e
dave <dub.cyclone@gmail.com>
2018-08-09 07:28:51 +0100

    Clean up refactor

--- UI/SkeletonGameManager.Module.Assets/ViewModels/AssetFileBaseViewModel.cs
+++ UI/SkeletonGameManager.Module.Assets/ViewModels/AssetFileBaseViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using GongSolutions.Wpf.DragDrop;
 using Prism.Events;
 using Prism.Logging;
-using Prism.Mvvm;
 using SkeletonGameManager.Base;
 using System;
 using System.Collections.ObjectModel;
@@ -12,26 +11,22 @@ namespace SkeletonGameManager.Module.Assets.ViewModels
 {
     public abstract class AssetFileBaseViewModel : SkeletonGameManagerViewModelBase, IDropTarget
     {
-        public ICommand OpenDirectoryCommand { get; set; }
-
-        public void OpenDirectory(string path)
-        {
-            if(!string.IsNullOrWhiteSpace(path))
-                FileFolder.Explore(path);
-        }
-
-        private ObservableCollection<string> assetFiles = new ObservableCollection<string>();
+        public ICommand OpenDirectoryCommand { get; set; }        
 
         public AssetFileBaseViewModel(IEventAggregator eventAggregator, ILoggerFacade loggerFacade) : base(eventAggregator, loggerFacade)
         {
         }
 
+        #region Properties
+        private ObservableCollection<string> assetFiles = new ObservableCollection<string>();
         public ObservableCollection<string> AssetFiles
         {
             get { return assetFiles; }
             set { SetProperty(ref assetFiles, value); }
-        }
+        } 
+        #endregion
 
+        #region Drag/Drop
         public virtual void DragOver(IDropInfo dropInfo)
         {
             throw new System.NotImplementedException();
@@ -40,11 +35,18 @@ namespace SkeletonGameManager.Module.Assets.ViewModels
         public virtual void Drop(IDropInfo dropInfo)
         {
             throw new System.NotImplementedException();
-        }
+        } 
+        #endregion
 
         public virtual Task GetFiles()
         {
             throw new NotImplementedException();
+        }
+
+        public void OpenDirectory(string path)
+        {
+            if (!string.IsNullOrWhiteSpace(path))
+                FileFolder.Explore(path);
         }
     }
 }
