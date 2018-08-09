810cd79a1fb6a5084c7b1da1d70a6b3fd40d4a89
dave <dub.cyclone@gmail.com>
2018-08-09 06:28:25 +0100

    Add log model and loglevel

---
+++ UI/SkeletonGameManager.Module.LogViewer/Model/LogLevel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkeletonGameManager.Module.LogViewer
+{
+    public enum LogLevel
+    {
+        Info,
+        Exception,
+        Debug,        
+        Warn
+    }
+}
