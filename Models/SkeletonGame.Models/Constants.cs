318080a1f5059fa3df806d7403a77625a5a583cd
dave <dub.cyclone@gmail.com>
2018-08-14 13:56:05 +0100

    Fix constant to be new_score_display

--- Models/SkeletonGame.Models/Constants.cs
+++ Models/SkeletonGame.Models/Constants.cs
@@ -8,6 +8,6 @@
         public const string FILE_SCOREDISPLAY = @"config/new_score_display.yaml";
         public const string FILE_MACHINE = @"config/machine.yaml";
         public const string FILE_TROPHY = @"config/trophy_default_data.yaml";
-        public const string FILE_NEWSCOREDISPLAY = @"config/score_display.yaml";
+        public const string FILE_NEWSCOREDISPLAY = @"config/new_score_display.yaml";
     }
 }
