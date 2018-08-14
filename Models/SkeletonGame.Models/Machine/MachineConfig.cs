a1d1ad5cac7528425f04bf1cccc048a75a112f8c
dave <dub.cyclone@gmail.com>
2018-08-14 13:55:31 +0100

    Create PrLampBase. Add PrLed. PrLeds and PRDriverGlobals

--- Models/SkeletonGame.Models/Machine/MachineConfig.cs
+++ Models/SkeletonGame.Models/Machine/MachineConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using YamlDotNet.Serialization;
 
 namespace SkeletonGame.Models.Machine
@@ -8,8 +9,9 @@ namespace SkeletonGame.Models.Machine
         [YamlMember(Alias = "name", ApplyNamingConventions = false, Order = 0)]
         public string Name { get; set; }
 
-        [YamlMember(Alias = "number", ApplyNamingConventions = false, Order = 1)]
-        public string Number { get; set; }
+        private string _number;
+        [YamlMember(Alias = "number", ApplyNamingConventions = false, Order = 1 )]
+        public string Number { get { return _number; } set { _number = value.ToUpper(); } }
 
         [YamlMember(Alias = "tags", ApplyNamingConventions = false, Order = 2, SerializeAs = typeof(string))]
         public string Tags { get; set; } = string.Empty;
@@ -21,6 +23,7 @@ namespace SkeletonGame.Models.Machine
     [PropertyChanged.AddINotifyPropertyChangedInterface]
     public class PRCoil : GameItemBase
     {
+
         [YamlMember(Alias = "pulseTime", ApplyNamingConventions = false)]
         public byte? PulseTime { get; set; }
 
@@ -37,10 +40,20 @@ namespace SkeletonGame.Models.Machine
         public SolenoidType? SolenoidType { get; set; }
     }
 
-    public class PRLamp : GameItemBase
+    public class PRLampBase : GameItemBase
     {
-        [YamlMember(Alias = "polarity", ApplyNamingConventions = false)]
-        public bool Polarity { get; set; }
+        [YamlMember(Alias = "polarity", ApplyNamingConventions = false, ScalarStyle = YamlDotNet.Core.ScalarStyle.Plain, SerializeAs = typeof(string))]
+        public bool Polarity { get; set; } = false;
+    }
+
+    public class PRLamp :  PRLampBase
+    {
+        
+    }
+
+    public class PRLed : PRLampBase
+    {
+
     }
 
     [PropertyChanged.AddINotifyPropertyChangedInterface]
@@ -66,7 +79,8 @@ namespace SkeletonGame.Models.Machine
     }   
 
     public class MachineConfig
-    {
+    {        
+        #region Properties
         [YamlMember(Alias = "PRGame", ApplyNamingConventions = false)]
         public PRGame PRGame { get; set; }
 
@@ -80,10 +94,63 @@ namespace SkeletonGame.Models.Machine
         public List<PRSwitch> PRSwitches { get; set; }
 
         [YamlMember(Alias = "PRLamps", ApplyNamingConventions = false)]
-        public List<PRLamp> PRLamps { get; set; }
+        public List<PRLamp> PRLamps { get; set; } = new List<PRLamp>();
 
         [YamlMember(Alias = "PRCoils", ApplyNamingConventions = false)]
         public List<PRCoil> PRCoils { get; set; }
+
+        [YamlMember(Alias = "PRLEDs", ApplyNamingConventions = false)]
+        public List<PRLed> PRLeds { get; set; }
+
+        [YamlMember(Alias = "PRDriverGlobals", ApplyNamingConventions = false)]
+        public PRDriverGlobal PRDriverGlobals { get; set; }
+
+        #endregion
+
+        /// <summary>
+        /// Gets the type of the configuration from machine types. wpc, stern or pdb
+        /// </summary>
+        /// <returns></returns>
+        public string GetConfigType()
+        {
+            var machineType = GetMachineType();
+            switch (machineType)
+            {
+                case MachineType.WPC:
+                case MachineType.WPC95:
+                case MachineType.WPCALPHANUMERIC:
+                    return "wpc";
+                case MachineType.STERNSAM:
+                case MachineType.STERNWHITESTAR:
+                    return "stern";
+                case MachineType.PDB:
+                    return "pdb";
+            }
+
+            return string.Empty;            
+        }
+
+        public MachineType GetMachineType()
+        {
+            return (MachineType)Enum.Parse(typeof(MachineType), PRGame?.MachineType.ToUpper());
+        }
+
+        #region Private Methods
+
+        #endregion
+    }
+
+
+    public class PRDriverGlobal
+    {
+        [YamlMember(Alias = "lamp_matrix_strobe_time", Order = 0, ApplyNamingConventions = false)]
+        public int MatrixStrobeTime { get; set; } = 200;
+
+        [YamlMember(Alias = "watchdog_time", Order = 0, ApplyNamingConventions = false)]
+        public int WatchdogTime { get; set; } = 1000;
+
+        [YamlMember(Alias = "use_watchdog", Order = 0, ApplyNamingConventions = false)]
+        public bool WatchdogEnabled { get; set; } = true;
     }
 
     public class MachineConfigDict
@@ -105,6 +172,9 @@ namespace SkeletonGame.Models.Machine
 
         [YamlMember(Alias = "PRCoils", Order = 5, ApplyNamingConventions = false)]        
         public Dictionary<string, PRCoil> PRCoils { get; set; }
+
+        [YamlMember(Alias = "PRLEDs", Order = 6, ApplyNamingConventions = false)]
+        public Dictionary<string, PRLed> PrLeds { get; set; }
 
     }
 }
