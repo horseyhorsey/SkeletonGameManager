85bcce9bde046f55954cf3f9940c1babdfc7ba8c
dave <dub.cyclone@gmail.com>
2018-08-14 14:31:50 +0100

    Create constructors to create from Lamp or LED

--- UI/SkeletonGameManager.Module.Config/ViewModels/Machine/LampViewModel.cs
+++ UI/SkeletonGameManager.Module.Config/ViewModels/Machine/LampViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace SkeletonGameManager.Module.Config.ViewModels.Machine
+﻿using SkeletonGame.Models.Machine;
+
+namespace SkeletonGameManager.Module.Config.ViewModels.Machine
 {
     [PropertyChanged.AddINotifyPropertyChangedInterface]
     public class LampViewModel
@@ -7,5 +9,26 @@
         public string Number { get; set; }
         public string Tags { get; set; }
         public string Label { get; set; }
+
+        public LampViewModel(PRLamp lamp)
+        {
+            Name = lamp.Name;
+            Number = lamp.Number;
+            Tags = lamp.Tags;
+            Label = lamp.Label;
+        }
+
+        public LampViewModel(PRLed led)
+        {
+            Name = led.Name;
+            Number = led.Number;
+            Tags = led.Tags;
+            Label = led.Label;
+        }
+
+        public LampViewModel()
+        {
+                
+        }                
     }
 }
