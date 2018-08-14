8cea304a13c7e5d70fc2b1d5bda4e3cfee2b458d
dave <dub.cyclone@gmail.com>
2018-08-14 14:41:30 +0100

    Add tests for VpGameMap

---
+++ Tests/SkeletonGame.Tests/VpGameMapTests.cs
@@ -0,0 +1,28 @@
+﻿using SkeletonGame.Engine;
+using System.Linq;
+using Xunit;
+
+namespace SkeletonGame.Tests
+{
+    public class VpGameMapTests
+    {
+        IVpGameMapper _vpGameMap;
+
+        public VpGameMapTests()
+        {
+            _vpGameMap = new VpGameMapper();
+        }
+
+        /// <summary>
+        /// Gets the mapping for the vp_game_map.yaml
+        /// </summary>
+        [Fact]
+        public void GetMapping()
+        {
+            var mappings = _vpGameMap.GetMappings(@"TestData\vp_game_map.yaml");            
+            Assert.NotNull(mappings);
+            Assert.True(mappings.Count() > 3);
+            Assert.True(!string.IsNullOrEmpty(mappings.ElementAt(0).Kls));
+        }
+    }
+}
