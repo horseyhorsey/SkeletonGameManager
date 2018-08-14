a3170eac6174792a33a954717753f2d2337d552b
dave <dub.cyclone@gmail.com>
2018-08-14 14:38:19 +0100

    Add Dialog Service implementation.

---
+++ UI/SkeletonGameManager.Module.Services/SgmDialogService.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Events;
+using Prism.Interactivity.InteractionRequest;
+using SkeletonGameManager.Base;
+
+namespace SkeletonGameManager.Module.Services
+{
+    public class SgmDialogService : ISgmDialogService
+    {
+        public InteractionRequest<INotification> NotificationRequest { get; private set; }
+
+        public void ShowDialog(string message)
+        {
+            NotificationRequest.Raise(new Notification
+            {
+                Content = "Notification Message",
+                Title = "Notification"               
+            });
+        }
+    }
+}
