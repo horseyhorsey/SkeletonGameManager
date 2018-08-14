25fb22a1734830f9feac24c13c35d3d6f8d39b6f
dave <dub.cyclone@gmail.com>
2018-08-14 14:18:14 +0100

    Refactor add rgb when reversing lampshows

--- Modules/SkeletonGame.Engine/LampshowEdit.cs
+++ Modules/SkeletonGame.Engine/LampshowEdit.cs
@@ -10,17 +10,18 @@ namespace SkeletonGame.Engine
         /// </summary>
         /// <param name="file">The file.</param>
         /// <param name="reversedFile">The reversed file to save</param>
-        void ReverseLampshowFile(string file, string reversedFile);
+        string ReverseLampshowFile(string file, string reversedFile);
     }
 
     public class LampshowEdit : ILampshowEdit
     {
         /// <summary>
-        /// Reverses a lampshow file.
+        /// Reverses a lampshow or rgb file.
         /// </summary>
         /// <param name="file">The file.</param>
         /// <param name="reversedFile">The reversed file to save</param>
-        public void ReverseLampshowFile(string file, string reversedFile)
+        /// <returns>The reversed lampshow path</returns>
+        public string ReverseLampshowFile(string file, string reversedFile)
         {
             using (var sr = File.OpenText(file))
             using (var sw = File.CreateText(reversedFile))
@@ -28,16 +29,31 @@ namespace SkeletonGame.Engine
                 string line = string.Empty;
                 while ((line = sr.ReadLine()) != null)
                 {
-                    if (line.StartsWith("lamp") || line.StartsWith("coil"))
+                    if (line.StartsWith("lamp") || line.StartsWith("coil") || line.StartsWith("rgb"))
                     {
-                        var split = line.Split('|');
-                        var splitreversed = new string(split[1].Reverse().ToArray()).Insert(0, " ");
-                        line = split[0] + "|" + splitreversed.Remove(splitreversed.Length - 1);
+                        line = SplitAndReversePipedTrack(line);
+                    }
+                    else if (line.Contains("|"))
+                    {
+                        if (!line.StartsWith("#"))
+                        {
+                            line = SplitAndReversePipedTrack(line);
+                        }
                     }
 
                     sw.WriteLine(line);
                 }
+
+                return Path.GetFileName(reversedFile);
             }
+        }
+
+        private string SplitAndReversePipedTrack(string line)
+        {
+            var split = line.Split('|');
+            var splitreversed = new string(split[1].Reverse().ToArray()).Insert(0, " ");
+            line = split[0] + "|" + splitreversed.Remove(splitreversed.Length - 1);
+            return line;
         }
     }
 }
