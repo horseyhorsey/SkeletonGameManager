b8c25d39dfdaff3dca34db47d658fe95eaf90048
dave <dub.cyclone@gmail.com>
2018-08-14 14:26:15 +0100

    Views now using View Discovery from container.

--- UI/SkeletonGameManager.Module.Assets/AssetsModule.cs
+++ UI/SkeletonGameManager.Module.Assets/AssetsModule.cs
@@ -16,9 +16,6 @@ namespace SkeletonGameManager.Module.Assets
 
         public void Initialize()
         {
-            _container.RegisterType<object, AssetListView>("AssetListView");
-            _container.RegisterType<object, AssetDetailsView>("AssetDetailsView");
-
             //Register these view models with container.            
             _container.RegisterInstance(_container.Resolve<AnimationsViewModel>(), new ContainerControlledLifetimeManager());
             _container.RegisterInstance(_container.Resolve<AssetDetailsViewModel>(), new ContainerControlledLifetimeManager());
@@ -27,8 +24,19 @@ namespace SkeletonGameManager.Module.Assets
             _container.RegisterInstance(_container.Resolve<LampshowViewModel>(), new ContainerControlledLifetimeManager());
             _container.RegisterInstance(_container.Resolve<LoadingProgressViewModel>(), new ContainerControlledLifetimeManager());
             _container.RegisterInstance(_container.Resolve<MusicViewModel>(), new ContainerControlledLifetimeManager());
-            _container.RegisterInstance(_container.Resolve<SfxViewModel>(), new ContainerControlledLifetimeManager());            
-            _container.RegisterInstance(_container.Resolve<VoiceViewModel>(), new ContainerControlledLifetimeManager());                       
+            _container.RegisterInstance(_container.Resolve<SfxViewModel>(), new ContainerControlledLifetimeManager());
+            _container.RegisterInstance(_container.Resolve<VoiceViewModel>(), new ContainerControlledLifetimeManager());
+
+            _container.RegisterType<object, AssetListView>("AssetListView");
+            _container.RegisterType<object, AssetDetailsView>("AssetDetailsView");
+
+            _container.RegisterType<object, AnimationsView>("AnimationsView");
+            _container.RegisterType<object, FontsView>("FontsView");
+            _container.RegisterType<object, LampshowView>("LampshowView");
+            _container.RegisterType<object, LoadingProgressView>("LoadingProgressView");
+            _container.RegisterType<object, MusicView>("MusicView");
+            _container.RegisterType<object, SfxView>("SfxView");
+            _container.RegisterType<object, VoiceView>("VoiceView");                 
         }
     }
 }
