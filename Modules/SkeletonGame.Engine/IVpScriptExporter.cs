75e4d5a58c75228b8c9b3dd3275996fa9211a3d5
dave <dub.cyclone@gmail.com>
2018-08-24 20:34:10 +0100

    Refactor, make pdb UseLeds by default. Fix PDB coil exporting. closes #14

--- Modules/SkeletonGame.Engine/IVpScriptExporter.cs
+++ Modules/SkeletonGame.Engine/IVpScriptExporter.cs
@@ -30,6 +30,7 @@ namespace SkeletonGame.Engine
     {
         private string _ballStackVariables = string.Empty;
 
+        #region Public Methods
         /// <summary>
         /// Creates a visual pinball script for the game
         /// </summary>
@@ -94,154 +95,33 @@ namespace SkeletonGame.Engine
             //Solenoid section
             templateScript = templateScript.Replace("{SOLENOID_SECTION}", $"{ExportMachineValuesToScript(config, VpScriptExportType.Coil)}");
 
+            if (config.PRLamps.Where(x => x.Name.ToUpper() != "NOT USED").Count() > 0)
+                templateScript = templateScript.Replace("{LAMP_SECTION}", GetLampScriptSection());
+            else
+            {
+                templateScript = templateScript.Replace("{LAMP_SECTION}", string.Empty);
+            }
+
+            if (config.PRGame.MachineType.ToUpper().Contains("WPC"))
+                templateScript = templateScript.Replace("{GI_SECTION}", GetGIScriptSection());
+            else
+                templateScript = templateScript.Replace("{GI_SECTION}", string.Empty);
+
+            if (config.PRGame.MachineType.ToUpper().Contains("PDB"))
+                templateScript = templateScript.Replace("{LED_SECTION}", GetLedScriptSection());
+            else
+                templateScript = templateScript.Replace("{LED_SECTION}", string.Empty);
+
             return templateScript;
-
-        }
-
-        private string CreateTroughScript(MachineConfig config, string dimName)
-        {
-            var troughScript = string.Empty;
-
-            var troughSwitches = config.PRSwitches.Where(x => x.Name.ToUpper().Contains("TROUGH")).Reverse();
-            _ballStackVariables += $"Dim bsTrough\r\n";
-            troughScript += $"Set {dimName} = New cvpmBallStack" + Environment.NewLine;
-
-            troughScript += $"{dimName}.InitSw ";
-            var troughSwitchList = "0";
-            var cnt = 1;
-            foreach (var trough in troughSwitches)
-            {
-                troughSwitchList += $", {trough.Number.Replace("S", "")}";
-                cnt++;
-            }
-
-            var zeroFill = 8 - cnt;
-            for (int i = 0; i < zeroFill; i++)
-            {
-                troughSwitchList += ", 0";
-            }
-
-            troughScript += troughSwitchList + Environment.NewLine;
-            troughScript += $"{dimName}.InitKick BallRelease, 90, 8" + Environment.NewLine;
-            troughScript += $"{dimName}.Balls = {config.PRGame.NumBalls}" + Environment.NewLine;
-            var dimQuoted = "\"" + dimName + "\"";
-            troughScript += $"{dimName}.CreateEvents {dimQuoted},  Drain" + Environment.NewLine;
-
-            return troughScript;
-        }
-
-        private string GetTemplateScript()
-        {
-            var assembly = Assembly.GetExecutingAssembly();
-            var resourceName = "SkeletonGame.Engine.VpScript.script_template.vbs";
-
-            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-            using (StreamReader reader = new StreamReader(stream))
-            {
-                return reader.ReadToEnd();
-            }
-        }
-
-        private string GetVpMachineScriptType(string machineType)
-        {
-            var procMachineType = Enum.Parse(typeof(MachineType), machineType.ToUpper());
-            switch (procMachineType)
-            {
-                case MachineType.WPC95:
-                case MachineType.WPC:
-                    return "WPC.vbs";
-                case MachineType.WPCALPHANUMERIC:
-                    return "S11.vbs";
-                case MachineType.STERNSAM:
-                    return "SAM.vbs";
-                case MachineType.STERNWHITESTAR:
-                    return "de.vbs";
-                case MachineType.PDB:
-                    return "PDB.vbs";
-            }
-
-            return null;
-        }
-
-        /// <summary>
-        /// Gets a VP scripted switch method.
-        /// </summary>
-        /// <param name="pRSwitch">The Machines switch</param>
-        /// <param name="procMachineType">Type of the PROC Board.</param>
-        /// <returns>String that contains a Hit() and UnHit() method invoking the Controller.Switch(num). If setting VP Switch type the controllers invocation will be different.</returns>
-        private string GetVpSwitchMethod(PRSwitch pRSwitch, MachineType procMachineType)
-        {
-            var exportString = $"' {pRSwitch.Name} hit {Environment.NewLine}";
-            string swNumber = null;
-            string swName = pRSwitch.Name;
-
-            //Get switch number with out any chars. Depending on machine type. Different with a PDB
-            if (procMachineType != MachineType.PDB)
-            {
-                swNumber = pRSwitch.Number.Replace("S", string.Empty);
-            }
-            else
-            {
-                swNumber = pRSwitch.Number.Split(':')[0];
-                if (swNumber.StartsWith("0"))
-                    swNumber = swNumber.Remove(0, 1);
-            }
-
-            // VP Pulse Switches
-            if (pRSwitch.VpSwitchType == VpSwitchType.PulseSwitch)
-            {
-                if (procMachineType != MachineType.PDB)
-                {
-                    exportString += $"' {swName} hit {Environment.NewLine}";
-                    exportString += $"Sub {swNumber}_Hit():vpmTimer.PulseSw {swNumber}:End Sub{Environment.NewLine}";
-                }
-            }
-            // VP Pulse Spinners
-            else if (pRSwitch.VpSwitchType == VpSwitchType.Spinner)
-            {
-                if (procMachineType != MachineType.PDB)
-                {
-                    exportString += $"' {swName} spin {Environment.NewLine}";
-                    exportString += $"Sub {swNumber}_Spin():vpmTimer.PulseSw {swNumber}:End Sub{Environment.NewLine}";
-                }
-            }
-            // Anything but BallStacks / Saucers
-            else if (pRSwitch.VpSwitchType != VpSwitchType.Saucer || pRSwitch.VpSwitchType != VpSwitchType.Vuk || pRSwitch.VpSwitchType != VpSwitchType.Scoop)
-            {
-                exportString += $"Sub sw{swNumber}_Hit():Controller.Switch({swNumber}) = 1 :End Sub{Environment.NewLine}";
-                exportString += $"Sub sw{swNumber}_UnHit():Controller.Switch({swNumber}) = 0 :End Sub{Environment.NewLine}";
-            }
-            return exportString;
-        }
-
-        private string GetVpSolCallback(PRCoil prCoil, MachineType procMachineType)
-        {
-            ///var exportString = $"' {pRSwitch.Name} hit {Environment.NewLine}";
-
-            //Create a string for vbscript
-            var solName = "Sol" + prCoil.Name;
-            var solStr = "\"" + solName + "\"";
-            string solNumber = null;
-
-            if (procMachineType != MachineType.PDB)
-            {
-                solNumber = prCoil.Number.Replace("C0", string.Empty);
-                solNumber = solNumber.Replace("C", string.Empty);
-            }
-            else
-            {
-                solNumber = prCoil.Number.Split(':')[0];
-                if (solNumber.StartsWith("0"))
-                    solNumber = solNumber.Remove(0, 1);
-            }
-            
-            return $"SolCallback({solNumber}) = {solStr}{Environment.NewLine}";
         }
 
         /// <summary>
         /// Exports the machine values to script from the given type. Creates Sub routines for Visual Basic Script <para />
-        /// Switch exports export Hit and UnHit Routines - Coils exports a list of SolCallbacks and empty sub routines to match
         /// </summary>
+        /// <remarks>
+        /// Switch exports > export Hit and UnHit Routines - Coils exports a list of SolCallbacks and empty sub routines to match.
+        /// VPSwitchTypes set to None won't be exported. This is used on things like the coin door, coins, service menu because the PDB.vbs assigns these to VP.
+        /// </remarks>
         /// <param name="machineConfig">The machine configuration.</param>
         /// <param name="exportType">Type of the export.</param>
         /// <returns></returns>
@@ -256,11 +136,17 @@ namespace SkeletonGame.Engine
                     exportString += $"' ** VP SWITCH EVENTS **  {newLine}";
                     foreach (var _switch in machineConfig.PRSwitches)
                     {
+                        if (_switch.VpSwitchType == VpSwitchType.None) continue;
+
                         //Don't export trough switches, flippers and dedicated
                         if (_switch.Name.ToUpper() != "NOT USED")
                             if (!_switch.Name.Contains("trough"))
                                 if (!_switch.Name.Contains("flipper"))
-                                    if (!_switch.Number.Contains("SD"))
+                                    if (machineConfig.GetMachineType() == MachineType.PDB)
+                                    {
+                                        exportString += GetVpSwitchMethod(_switch, machineConfig.GetMachineType());
+                                    }
+                                    else if (!_switch.Number.Contains("SD"))
                                     {
                                         exportString += GetVpSwitchMethod(_switch, machineConfig.GetMachineType());
                                     }
@@ -312,7 +198,9 @@ namespace SkeletonGame.Engine
 
             return exportString;
         }
+        #endregion
 
+        #region Private methods
         /// <summary>
         /// Creates a ball stack script for use in the VBS constructor
         /// </summary>
@@ -358,5 +246,188 @@ namespace SkeletonGame.Engine
 
             return ballStackScript;
         }
+
+        private string CreateTroughScript(MachineConfig config, string dimName)
+        {
+            var troughScript = string.Empty;
+
+            var troughSwitches = config.PRSwitches.Where(x => x.Name.ToUpper().Contains("TROUGH")).Reverse();
+            _ballStackVariables += $"Dim bsTrough\r\n";
+            troughScript += $"Set {dimName} = New cvpmBallStack" + Environment.NewLine;
+
+            troughScript += $"{dimName}.InitSw ";
+            var troughSwitchList = "0";
+            var cnt = 1;
+            foreach (var trough in troughSwitches)
+            {
+                troughSwitchList += $", {trough.Number.Replace("S", "")}";
+                cnt++;
+            }
+
+            var zeroFill = 8 - cnt;
+            for (int i = 0; i < zeroFill; i++)
+            {
+                troughSwitchList += ", 0";
+            }
+
+            troughScript += troughSwitchList + Environment.NewLine;
+            troughScript += $"{dimName}.InitKick BallRelease, 90, 8" + Environment.NewLine;
+            troughScript += $"{dimName}.Balls = {config.PRGame.NumBalls}" + Environment.NewLine;
+            var dimQuoted = "\"" + dimName + "\"";
+            troughScript += $"{dimName}.CreateEvents {dimQuoted},  Drain" + Environment.NewLine;
+
+            return troughScript;
+        }
+
+        private string GetLampScriptSection()
+        {
+            string exportString = string.Empty;
+
+            exportString += $"Const UseLamps = True ' This enables the Core.vbs to update {Environment.NewLine}";            
+            var quote = "\"";
+            exportString += $"Set LampCallback = GetRef({quote}UpdateMultipleLamps{quote}) ' This empty callback uses Core.vbs do the lamp updates";
+            exportString += $"Sub UpdateMultipleLamps : End Sub ' Use this to do other things when lamps changed";
+
+            return exportString;
+        }
+
+        private string GetLedScriptSection()
+        {
+            string exportString = string.Empty;
+            exportString += $"Const UsePdLeds = True ' This enables the Core.vbs to update Leds {Environment.NewLine}";
+            var quote = "\"";
+            exportString += $"Set PDLedCallback = GetRef({quote}UpdateLeds{quote}) ' This empty callback uses Core.vbs do the lamp updates";
+            exportString += $"Sub UpdateLeds : End Sub ' Use this to do other things when leds changed";
+            return exportString;
+        }
+
+        private string GetGIScriptSection()
+        {
+            string exportString = string.Empty;
+            var quote = "\"";
+            exportString += $"Dim GIon, lamp ' Williams has GI you can use from proc{Environment.NewLine}";
+            exportString += $"Set GICallback = GetRef({quote}UpdateGI{quote}) ' This empty callback uses Core.vbs do the lamp updates";
+            exportString += $"Sub UpdateGI(giNo, stat)";
+            exportString += $"  Select Case giNo{Environment.NewLine}    Case 0 GI_low(abs(stat)){Environment.NewLine}";
+            exportString += $"    Case 1 GI_mid(abs(stat)){Environment.NewLine}    Case 2 GI_hi(abs(stat)){Environment.NewLine}  End Select";
+            exportString += $"End Sub{Environment.NewLine}{Environment.NewLine}";
+
+            exportString += $"Sub GI_low(enabled) : End Sub{Environment.NewLine}";
+            exportString += $"Sub GI_mid(enabled) : End Sub{Environment.NewLine}";
+            exportString += $"Sub GI_hi(enabled) : End Sub{Environment.NewLine}";
+
+
+            return exportString;
+        }
+
+        private string GetVpMachineScriptType(string machineType)
+        {
+            var procMachineType = Enum.Parse(typeof(MachineType), machineType.ToUpper());
+            switch (procMachineType)
+            {
+                case MachineType.WPC95:
+                case MachineType.WPC:
+                    return "WPC.vbs";
+                case MachineType.WPCALPHANUMERIC:
+                    return "S11.vbs";
+                case MachineType.STERNSAM:
+                    return "SAM.vbs";
+                case MachineType.STERNWHITESTAR:
+                    return "de.vbs";
+                case MachineType.PDB:
+                    return "PDB.vbs";
+            }
+
+            return null;
+        }
+
+        private string GetTemplateScript()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "SkeletonGame.Engine.VpScript.script_template.vbs";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        private string GetVpSolCallback(PRCoil prCoil, MachineType procMachineType)
+        {
+            ///var exportString = $"' {pRSwitch.Name} hit {Environment.NewLine}";
+
+            //Create a string for vbscript
+            var solName = "Sol" + prCoil.Name;
+            var solStr = "\"" + solName + "\"";
+            string solNumber = null;
+
+            if (procMachineType != MachineType.PDB)
+            {
+                solNumber = prCoil.Number.Replace("C0", string.Empty);
+                solNumber = solNumber.Replace("C", string.Empty);
+            }
+            else
+            {
+                //This only good if exporting from the UI
+                solNumber = prCoil.Number.Split(':')[0];
+                if (solNumber.StartsWith("0"))
+                    solNumber = solNumber.Remove(0, 1);
+            }
+
+            return $"SolCallback({solNumber}) = {solStr}{Environment.NewLine}";
+        }
+
+        /// <summary>
+        /// Gets a VP scripted switch method.
+        /// </summary>
+        /// <param name="pRSwitch">The Machines switch</param>
+        /// <param name="procMachineType">Type of the PROC Board.</param>
+        /// <returns>String that contains a Hit() and UnHit() method invoking the Controller.Switch(num). If setting VP Switch type the controllers invocation will be different.</returns>
+        private string GetVpSwitchMethod(PRSwitch pRSwitch, MachineType procMachineType)
+        {
+            var exportString = $"' {pRSwitch.Name} hit {Environment.NewLine}";
+            string swNumber = null;
+            string swName = pRSwitch.Name;
+
+            //Get switch number with out any chars. Depending on machine type. Different with a PDB
+            if (procMachineType != MachineType.PDB)
+            {
+                swNumber = pRSwitch.Number.Replace("S", string.Empty);
+            }
+            else
+            {
+                swNumber = pRSwitch.Number.Replace("SD", string.Empty);
+                if (swNumber.StartsWith("0"))
+                    swNumber = swNumber.Remove(0, 1);
+            }
+
+            // VP Pulse Switches
+            if (pRSwitch.VpSwitchType == VpSwitchType.PulseSwitch)
+            {
+                if (procMachineType != MachineType.PDB)
+                {
+                    exportString += $"' {swName} hit {Environment.NewLine}";
+                    exportString += $"Sub {swNumber}_Hit():vpmTimer.PulseSw {swNumber}:End Sub{Environment.NewLine}";
+                }
+            }
+            // VP Pulse Spinners
+            else if (pRSwitch.VpSwitchType == VpSwitchType.Spinner)
+            {
+                if (procMachineType != MachineType.PDB)
+                {
+                    exportString += $"' {swName} spin {Environment.NewLine}";
+                    exportString += $"Sub {swNumber}_Spin():vpmTimer.PulseSw {swNumber}:End Sub{Environment.NewLine}";
+                }
+            }
+            // Anything but BallStacks / Saucers
+            else if (pRSwitch.VpSwitchType != VpSwitchType.Saucer || pRSwitch.VpSwitchType != VpSwitchType.Vuk || pRSwitch.VpSwitchType != VpSwitchType.Scoop)
+            {
+                exportString += $"Sub sw{swNumber}_Hit():Controller.Switch({swNumber}) = 1 :End Sub{Environment.NewLine}";
+                exportString += $"Sub sw{swNumber}_UnHit():Controller.Switch({swNumber}) = 0 :End Sub{Environment.NewLine}";
+            }
+            return exportString;
+        } 
+        #endregion
     }
 }
