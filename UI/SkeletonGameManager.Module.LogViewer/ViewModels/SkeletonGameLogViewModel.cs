225e53cd6975a168471da050bdd63facdf6e86f0
dave <dub.cyclone@gmail.com>
2018-08-26 22:49:42 +0100

    Added filtering to the SkeletonGameLogs View

--- UI/SkeletonGameManager.Module.LogViewer/ViewModels/SkeletonGameLogViewModel.cs
+++ UI/SkeletonGameManager.Module.LogViewer/ViewModels/SkeletonGameLogViewModel.cs
@@ -2,29 +2,69 @@
 using Prism.Logging;
 using SkeletonGameManager.Base;
 using System;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows.Data;
 using static SkeletonGameManager.Base.Events;
 
 namespace SkeletonGameManager.Module.LogViewer.ViewModels
 {
     public class SkeletonGameLogViewModel : LogViewModelBase
     {
-        private ISkeletonGameProvider _skeletonGameProvider;
+        #region Fields
+        private ISkeletonGameProvider _skeletonGameProvider; 
+        #endregion
 
+        #region Constructors
         public SkeletonGameLogViewModel(ISkeletonGameProvider skeletonGameProvider, IEventAggregator eventAggregator, ILoggerFacade loggerFacade) : base(eventAggregator, loggerFacade)
         {
             Title = "Game logs";
             this.LogLines = new System.Collections.ObjectModel.ObservableCollection<LogViewer.Log>();
+            LogLinesCollectionView = new ListCollectionView(this.LogLines);
 
             _skeletonGameProvider = skeletonGameProvider;
 
             _eventAggregator.GetEvent<LoadYamlFilesChanged>().Subscribe(async x => await OnLoadYamlFilesChanged());
 
             _eventAggregator.GetEvent<OnGameEndedEvent>().Subscribe(OnGameEnded);
+        } 
+        #endregion
+
+        #region Properties
+        public ICollectionView LogLinesCollectionView { get; }
+
+        private bool _caseSensitiveFilter = false;
+        /// <summary>
+        /// Gets or sets a value indicating whether using a case sensitive filter search
+        /// </summary>
+        public bool CaseSensitiveFilter
+        {
+            get { return _caseSensitiveFilter; }
+            set { SetProperty(ref _caseSensitiveFilter, value); }
         }
 
+        private string _logLineFilterText;
+        /// <summary>
+        /// Gets or sets the log line filter text.
+        /// </summary>
+        public string LogLineFilterText
+        {
+            get { return _logLineFilterText; }
+            set
+            {
+                var set = SetProperty(ref _logLineFilterText, value);
+                if (set)
+                {
+                    UpdateLogLinesFilter();
+                }
+            }
+        }
+
+        #endregion
+
+        #region Public methods
         /// <summary>
         /// Called when /[load yaml files changed]. Clears the logLines, populates logs from directory.
         /// </summary>
@@ -36,8 +76,10 @@ namespace SkeletonGameManager.Module.LogViewer.ViewModels
             GetLogs();
 
             return Task.CompletedTask;
-        }
+        } 
+        #endregion
 
+        #region Private Methods
         protected override void GetLogs()
         {
             Log("Populating logs");
@@ -66,7 +108,7 @@ namespace SkeletonGameManager.Module.LogViewer.ViewModels
         protected override void UpdateFromSelected()
         {
             try
-            {                
+            {
                 this.LogLines?.Clear();
                 var content = File.ReadLines(Path.Combine(this.LogPath, this.SelectedLogFile));
                 foreach (var line in content)
@@ -81,5 +123,35 @@ namespace SkeletonGameManager.Module.LogViewer.ViewModels
                 LogLines?.Clear();
             }
         }
+
+        private void UpdateLogLinesFilter()
+        {
+            this.LogLinesCollectionView.Filter = (f) =>
+            {
+                string line = string.Empty;
+                if (CaseSensitiveFilter)
+                    line = (f as Log).Line;
+                else
+                    line = (f as Log).Line.ToUpper();
+
+                if (CaseSensitiveFilter)
+                {
+                    if (line.Contains(LogLineFilterText))
+                        return true;
+                    else
+                        return false;
+                }
+                else
+                {
+                    if (line.Contains(LogLineFilterText.ToUpper()))
+                        return true;
+                    else
+                        return false;
+                }
+            };
+
+            this.LogLinesCollectionView.Refresh();
+        } 
+        #endregion
     }
 }
